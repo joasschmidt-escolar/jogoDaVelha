dd70311ecddc30933db2ac9c7a328bb14e6ed1f1
joasschmidt-escolar <joas_schmidt@estudante.sesisenai.org.br>
2026-04-07 16:36:38 -0300

    versão sem velha

---
+++ .history/JogoDaVelha/app/src/App_20260407162311.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String tabuleiro [][] = new String[3][3];
+        String x = "X", o = "O";
+        boolean win = true;
+        int jogadorXLinha, jogadorXColuna, jogadorOLinha, jogadorOColuna;
+        //tabuleiro
+        for(int cont = 0; cont < 3; cont++){
+            for(int cont1 = 0; cont1 < 3; cont1++){
+                tabuleiro[cont][cont1] = "#";
+            }
+        }
+        System.out.println(tabuleiro[0][0] + "|" + tabuleiro[0][1] + "|" + tabuleiro[0][2]+ "\n" + tabuleiro[1][0] + "|" + tabuleiro[1][1] + "|" + tabuleiro[1][2]+ "\n" + tabuleiro[2][0] + "|" + tabuleiro[2][1] + "|" + tabuleiro[2][2]);
+        while (win) {
+            //jogadorX
+            System.out.println("Digite a linha que quer posicionar o X: ");
+            jogadorXLinha = sc.nextInt();
+            System.out.println("Digite a coluna que quer posicionar o X: ");
+            jogadorXColuna = sc.nextInt();
+            tabuleiro[jogadorXLinha][jogadorXColuna] = x;
+            System.out.println(tabuleiro[0][0] + "|" + tabuleiro[0][1] + "|" + tabuleiro[0][2]+ "\n" + tabuleiro[1][0] + "|" + tabuleiro[1][1] + "|" + tabuleiro[1][2]+ "\n" + tabuleiro[2][0] + "|" + tabuleiro[2][1] + "|" + tabuleiro[2][2]);
+            //jogadorO
+            System.out.println("Digite a linha que quer posicionar o O: ");
+            jogadorOLinha = sc.nextInt();
+            System.out.println("Digite a coluna que quer posicionar o O: ");
+            jogadorOColuna = sc.nextInt();
+            tabuleiro[jogadorOLinha][jogadorOColuna] = o;
+            System.out.println(tabuleiro[0][0] + "|" + tabuleiro[0][1] + "|" + tabuleiro[0][2]+ "\n" + tabuleiro[1][0] + "|" + tabuleiro[1][1] + "|" + tabuleiro[1][2]+ "\n" + tabuleiro[2][0] + "|" + tabuleiro[2][1] + "|" + tabuleiro[2][2]);
+            //verificar vitória
+            for(int cont = 0; cont < 3; cont++){
+                if((tabuleiro[cont][0] == tabuleiro[cont][1]) && tabuleiro[cont][1] == tabuleiro[cont][2]){
+                    if(tabuleiro.equals(x)){
+                        System.out.println("Jogador X Ganhou!!!!!\n");
+                        win = false;
+                    } else if(tabuleiro.equals(o)){
+                        System.out.println("Jogador O Ganhou!!!!!\n");
+                        win = false;
+                    }
+                }
+            }
+            for(int cont = 0; cont < 3; cont++){
+                if((tabuleiro[0][cont] == tabuleiro[1][cont]) && tabuleiro[1][cont] == tabuleiro[2][cont]){
+                    if(tabuleiro[0][cont].equals(x)){
+                        System.out.println("Jogador X Ganhou!!!!!\n");
+                        win = false;
+                    } else if(tabuleiro.equals(o)){
+                        System.out.println("Jogador O Ganhou!!!!!\n");
+                        win = false;
+                    }
+                }
+            }
+            if((tabuleiro[0][0] == tabuleiro[1][1]) && tabuleiro[1][1] == tabuleiro[2][2]){
+                if(tabuleiro[0][0].equals(x)){
+                    System.out.println("Jogador X Ganhou!!!!!\n");
+                    win = false;
+                } else if(tabuleiro[0][0].equals(o)){
+                    System.out.println("Jogador O Ganhou!!!!!\n");
+                    win = false;
+                }
+            }
+            if((tabuleiro[0][2] == tabuleiro[1][1]) && tabuleiro[1][1] == tabuleiro[2][0]){
+                if(tabuleiro[0][2].equals(x)){
+                    System.out.println("Jogador X Ganhou!!!!!\n");
+                    win = false;
+                } else if(tabuleiro[0][2].equals(o)){
+                    System.out.println("Jogador O Ganhou!!!!!\n");
+                    win = false;
+                }
+            }
+        }
+    }
+}
--- JogoDaVelha/app/src/App.java
+++ JogoDaVelha/app/src/App.java
@@ -16,50 +16,95 @@ public class App {
         System.out.println(tabuleiro[0][0] + "|" + tabuleiro[0][1] + "|" + tabuleiro[0][2]+ "\n" + tabuleiro[1][0] + "|" + tabuleiro[1][1] + "|" + tabuleiro[1][2]+ "\n" + tabuleiro[2][0] + "|" + tabuleiro[2][1] + "|" + tabuleiro[2][2]);
         while (win) {
             //jogadorX
-            System.out.println("Digite a linha que quer posicionar o X: ");
-            jogadorXLinha = sc.nextInt();
-            System.out.println("Digite a coluna que quer posicionar o X: ");
-            jogadorXColuna = sc.nextInt();
-            tabuleiro[jogadorXLinha][jogadorXColuna] = "X";
-            System.out.println(tabuleiro[0][0] + "|" + tabuleiro[0][1] + "|" + tabuleiro[0][2]+ "\n" + tabuleiro[1][0] + "|" + tabuleiro[1][1] + "|" + tabuleiro[1][2]+ "\n" + tabuleiro[2][0] + "|" + tabuleiro[2][1] + "|" + tabuleiro[2][2]);
-            //jogadorO
-            System.out.println("Digite a linha que quer posicionar o O: ");
-            jogadorOLinha = sc.nextInt();
-            System.out.println("Digite a coluna que quer posicionar o O: ");
-            jogadorOColuna = sc.nextInt();
-            tabuleiro[jogadorOLinha][jogadorOColuna] = "O";
-            System.out.println(tabuleiro[0][0] + "|" + tabuleiro[0][1] + "|" + tabuleiro[0][2]+ "\n" + tabuleiro[1][0] + "|" + tabuleiro[1][1] + "|" + tabuleiro[1][2]+ "\n" + tabuleiro[2][0] + "|" + tabuleiro[2][1] + "|" + tabuleiro[2][2]);
-            //verificar vitória
+            if(win){
+                System.out.println("Digite a linha que quer posicionar o X: ");
+                jogadorXLinha = sc.nextInt();
+                System.out.println("Digite a coluna que quer posicionar o X: ");
+                jogadorXColuna = sc.nextInt();
+                tabuleiro[jogadorXLinha][jogadorXColuna] = x;
+                System.out.println(tabuleiro[0][0] + "|" + tabuleiro[0][1] + "|" + tabuleiro[0][2]+ "\n" + tabuleiro[1][0] + "|" + tabuleiro[1][1] + "|" + tabuleiro[1][2]+ "\n" + tabuleiro[2][0] + "|" + tabuleiro[2][1] + "|" + tabuleiro[2][2]);
+            }
+            //verificar vitória 1
             for(int cont = 0; cont < 3; cont++){
                 if((tabuleiro[cont][0] == tabuleiro[cont][1]) && tabuleiro[cont][1] == tabuleiro[cont][2]){
-                    if(tabuleiro.equals(x)){
-                        System.out.println("Jogador X Ganhou!!!!!\n");
-                    } else if(tabuleiro.equals(o)){
-                        System.out.println("Jogador O Ganhou!!!!!\n");
+                    if(tabuleiro[cont][0].equals(x)){
+                        win = false;
+                    } else if(tabuleiro[cont][0].equals(o)){
+                        win = false;
                     }
                 }
             }
             for(int cont = 0; cont < 3; cont++){
                 if((tabuleiro[0][cont] == tabuleiro[1][cont]) && tabuleiro[1][cont] == tabuleiro[2][cont]){
-                    if(tabuleiro.equals(x)){
-                        System.out.println("Jogador X Ganhou!!!!!\n");
-                    } else if(tabuleiro.equals(o)){
-                        System.out.println("Jogador O Ganhou!!!!!\n");
+                    if(tabuleiro[0][cont].equals(x)){
+                        win = false;
+                    } else if(tabuleiro[0][cont].equals(o)){
+                        win = false;
                     }
                 }
             }
             if((tabuleiro[0][0] == tabuleiro[1][1]) && tabuleiro[1][1] == tabuleiro[2][2]){
-                if(tabuleiro.equals(x)){
-                    System.out.println("Jogador X Ganhou!!!!!\n");
-                } else if(tabuleiro.equals(o)){
-                    System.out.println("Jogador O Ganhou!!!!!\n");
+                if(tabuleiro[0][0].equals(x)){
+                    win = false;
+                } else if(tabuleiro[0][0].equals(o)){
+                    win = false;
                 }
             }
             if((tabuleiro[0][2] == tabuleiro[1][1]) && tabuleiro[1][1] == tabuleiro[2][0]){
-                if(tabuleiro.equals(x)){
+                if(tabuleiro[0][2].equals(x)){
+                    win = false;
+                } else if(tabuleiro[0][2].equals(o)){
+                    win = false;
+                }
+            }
+            //jogadorO
+            if(win){
+                System.out.println("Digite a linha que quer posicionar o O: ");
+                jogadorOLinha = sc.nextInt();
+                System.out.println("Digite a coluna que quer posicionar o O: ");
+                jogadorOColuna = sc.nextInt();
+                tabuleiro[jogadorOLinha][jogadorOColuna] = o;
+                System.out.println(tabuleiro[0][0] + "|" + tabuleiro[0][1] + "|" + tabuleiro[0][2]+ "\n" + tabuleiro[1][0] + "|" + tabuleiro[1][1] + "|" + tabuleiro[1][2]+ "\n" + tabuleiro[2][0] + "|" + tabuleiro[2][1] + "|" + tabuleiro[2][2]);
+            }
+            //verificar vitória 2
+            for(int cont = 0; cont < 3; cont++){
+                if((tabuleiro[cont][0] == tabuleiro[cont][1]) && tabuleiro[cont][1] == tabuleiro[cont][2]){
+                    if(tabuleiro[cont][0].equals(x)){
+                        System.out.println("Jogador X Ganhou!!!!!\n");
+                        win = false;
+                    } else if(tabuleiro[cont][0].equals(o)){
+                        System.out.println("Jogador O Ganhou!!!!!\n");
+                        win = false;
+                    }
+                }
+            }
+            for(int cont = 0; cont < 3; cont++){
+                if((tabuleiro[0][cont] == tabuleiro[1][cont]) && tabuleiro[1][cont] == tabuleiro[2][cont]){
+                    if(tabuleiro[0][cont].equals(x)){
+                        System.out.println("Jogador X Ganhou!!!!!\n");
+                        win = false;
+                    } else if(tabuleiro[0][cont].equals(o)){
+                        System.out.println("Jogador O Ganhou!!!!!\n");
+                        win = false;
+                    }
+                }
+            }
+            if((tabuleiro[0][0] == tabuleiro[1][1]) && tabuleiro[1][1] == tabuleiro[2][2]){
+                if(tabuleiro[0][0].equals(x)){
                     System.out.println("Jogador X Ganhou!!!!!\n");
-                } else if(tabuleiro.equals(o)){
+                    win = false;
+                } else if(tabuleiro[0][0].equals(o)){
                     System.out.println("Jogador O Ganhou!!!!!\n");
+                    win = false;
+                }
+            }
+            if((tabuleiro[0][2] == tabuleiro[1][1]) && tabuleiro[1][1] == tabuleiro[2][0]){
+                if(tabuleiro[0][2].equals(x)){
+                    System.out.println("Jogador X Ganhou!!!!!\n");
+                    win = false;
+                } else if(tabuleiro[0][2].equals(o)){
+                    System.out.println("Jogador O Ganhou!!!!!\n");
+                    win = false;
                 }
             }
         }
